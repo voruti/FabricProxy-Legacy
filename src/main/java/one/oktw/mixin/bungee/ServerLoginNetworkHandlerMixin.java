b23eb9cf35e0403520ce81b237c0b348bdc82161
voruti <45130096+voruti@users.noreply.github.com>
2023-12-01 21:36:41 +0100

    Update ServerLoginNetworkHandlerMixin.java

--- src/main/java/one/oktw/mixin/bungee/ServerLoginNetworkHandlerMixin.java
+++ src/main/java/one/oktw/mixin/bungee/ServerLoginNetworkHandlerMixin.java
@@ -20,7 +20,7 @@ public abstract class ServerLoginNetworkHandlerMixin {
 
     @Shadow
     @Final
-    public ClientConnection connection;
+    ClientConnection connection;
 
     @Shadow
     private GameProfile profile;
