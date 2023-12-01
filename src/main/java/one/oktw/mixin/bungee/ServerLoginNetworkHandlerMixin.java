45ba1d12e5b2d26ec223ee46d10234dac1cfacf1
voruti <45130096+voruti@users.noreply.github.com>
2023-12-01 22:18:06 +0100

    Fix

--- src/main/java/one/oktw/mixin/bungee/ServerLoginNetworkHandlerMixin.java
+++ src/main/java/one/oktw/mixin/bungee/ServerLoginNetworkHandlerMixin.java
@@ -23,8 +23,10 @@ public abstract class ServerLoginNetworkHandlerMixin {
     @Redirect(method = "onHello", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/server/MinecraftServer;getHostProfile()Lcom/mojang/authlib/GameProfile;"))
     private GameProfile initUuid(MinecraftServer minecraftServer) {
+        final GameProfile originalGameProfile = minecraftServer.getHostProfile();
+
         // override game profile with saved information:
-        final GameProfile gameProfile = new GameProfile(((BungeeClientConnection) connection).getSpoofedUUID(), this.profile.getName());
+        final GameProfile gameProfile = new GameProfile(((BungeeClientConnection) connection).getSpoofedUUID(), originalGameProfile.getName());
 
         if (((BungeeClientConnection) connection).getSpoofedProfile() != null) {
             for (Property property : ((BungeeClientConnection) connection).getSpoofedProfile()) {
