937ea1c2713c65d9856025d0ec6f95253991de2c
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2024-03-07 02:18:13 -0300

    (feat): 1.20.4 minecraft support
    Co-Authored-By: alikindsys <roridev@protonmail.com>

--- src/main/java/one/oktw/mixin/UserCacheMixin.java
+++ src/main/java/one/oktw/mixin/UserCacheMixin.java
@@ -1,14 +1,17 @@
 package one.oktw.mixin;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.util.UserCache;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Optional;
+
 @Mixin(UserCache.class)
 public class UserCacheMixin {
-    @Redirect(method = "findProfileByName", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/UserCache;shouldUseRemote()Z"))
-    private static boolean findProfileByName() {
-        return true;
+    @Redirect(method = "findProfileByName", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/UserCache;getOfflinePlayerProfile(Ljava/lang/String;)Ljava/util/Optional;"))
+    private static Optional<GameProfile> getOfflinePlayerProfile(String string) {
+        return Optional.empty();
     }
 }
